702b4a599b5b39b64f485c526995cb612263dad3
Oleg Shchelykalnov <olegshtch@yandex.ru>
2015-07-13 00:42:24 +0300

    Add downloading and unpacking data.

--- plot3.R
+++ plot3.R
@@ -1,4 +1,7 @@
 get_data <- function() {
+	download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+								, "household_power_consumption.zip", method = "wget", extra = "-c")
+	unzip("household_power_consumption.zip")
 	t <- read.csv("household_power_consumption.txt", header = TRUE, sep = ";"
 									, na.strings = "?")
 	t[t$Date == "1/2/2007" | t$Date == "2/2/2007", ]
